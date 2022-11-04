30f32c53550224d55a494b0bcca7a7a8bf24ba94
Koketso Makaleng <fozapretunate@gmail.com>
2022-11-04 12:31:24 +0200

    Added the greet.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import greet from './greet.js';
+import chalk from 'chalk';
+
+import fun from 'everyday-fun';
+import figlet from 'figlet';
+
+figlet('...Riddler...', function (err, result) {
+    console.log(chalk.blue.bgBlackBright.bold(result));
+
+    const guess = fun.getRandomRiddle();
+
+    console.log(chalk.bgGreen(guess.riddle));
+
+    setTimeout(function () {
+        console.log(chalk.blue.bgBlack.bold(guess.answer));
+    }, 5000);
+
+});
+
